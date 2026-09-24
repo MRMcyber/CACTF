ccd4e276f5c4aab39a239795366dd85b374c83fd
Moyin <gbadelawalemmanuel@gmail.com>
2026-09-24 13:03:22 +0100

    fix: update api/index.js and vercel.json for Neon Postgres (remove sql.js/ctf.db refs)

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 const app = require('../server');
-const { initSQL } = require('../db/database');
-
-const ready = initSQL();
 
 module.exports = async (req, res) => {
-  await ready;
   return app(req, res);
 };
